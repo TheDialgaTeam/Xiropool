adf46b2750ede6082c74513dd9d95cffda26381c
XIROPHT <37265696+XIROPHT@users.noreply.github.com>
2019-04-21 14:29:26 +0200

    Upload example website
    -> Upload example of website compatible with the pool.

---
+++ website_example/config.js
@@ -0,0 +1,7 @@
+var apiHost = "https://pool-api.xiropht.com/";
+
+var coinMinName = "XIR";
+	
+var transactionExplorer = "https://xiropht.com/transaction-explorer.html#";
+
+var blockExplorer = "https://xiropht.com/blockchain-explorer.html#";
